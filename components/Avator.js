a16f4e551d996e399e48da9ab13b9895dfdbf6d3
Sahandgha <sahand.gha@gmail.com>
2022-02-22 08:12:42 +1000

    create footer component

--- components/Avator.js
+++ components/Avator.js
@@ -5,8 +5,8 @@ export default function Avator() {
     <div className="h-12 w-12 rounded-full overflow-hidden cursor-pointer">
       <Image
         src="https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048"
-        width={400}
-        height={400}
+        width={100}
+        height={100}
         objectFit="cover"
       />
     </div>
--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Avator from "./Avator";
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-5 text-sm text-gray-700">
+    <header className="flex justify-between p-5 text-sm text-gray-700 w-full">
       {/* left */}
       <div className="flex space-x-4 items-center">
         <p className="link">About</p>
